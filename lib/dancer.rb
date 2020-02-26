3d7f3cc929fc99d7027ff9b7e22d6ce1f9b62be2
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-26 18:32:09 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,17 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+<<<<<<< HEAD
 require_relative "./fancy_dance.rb"
 
 class Dancer 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+=======
+
+class Dancer 
+  include Dance
+  extend MetaDancing
+>>>>>>> ee1fffc7b6ca37b2120ef79f12a4bcedd5ef7406
  
   attr_accessor :name
  
--- lib/kid.rb
+++ lib/kid.rb
@@ -1,10 +1,17 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
+<<<<<<< HEAD
 require_relative "./fancy_dance.rb"
 
 class Kid 
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
+=======
+
+class Kid 
+  include Dance
+  extend MetaDancing
+>>>>>>> ee1fffc7b6ca37b2120ef79f12a4bcedd5ef7406
  
   attr_accessor :name
  
